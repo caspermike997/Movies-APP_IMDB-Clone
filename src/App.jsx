d4e89aa9acfcdd9cc562a70c6ea6a9aaee7215c6
caspermike997 <caspermike97@gmail.com>
2023-09-14 13:25:48 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='App'>
+      <div className='search-nav'>
+        <div>
+          <h1>Movies</h1>
+        </div>
+
+        <div>
+          <form>
+            <input type="text" />
+            <button>Search</button>
+          </form>
+        </div>
+      </div>
+
+
+      <div className='movies'>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
