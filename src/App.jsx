e7b67cf9387f121a8678b3b32ec257b0b4d08ce0
caspermike997 <caspermike97@gmail.com>
2023-09-14 14:48:59 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import MovieCard from './MovieCard'
 
 function App() {
 
+const server= "https://api.themoviedb.org/3/movie"
+const API_KEY = "8e7a7d97fefd8b47be7bd7039b7aeb23"
+const API_URL =`${server}/popular?api_key=${API_KEY}&language=en-US&page=1`
+
+  const [movies, setMovies]  = useState([])
+
+  useEffect (() =>{
+      fetch(API_URL)
+      .then(res => res.json())
+      .then(data  => setMovies(data))
+  },[])
+console.log(movies);
 
   return (
     <div className='App'>
@@ -19,9 +33,11 @@ function App() {
       </div>
 
 
-      <div className='movies'>
-
-      </div>
+      {/* <div className='movies'>
+        {movies.map((movie)=>{
+          <MovieCard />
+        })}
+      </div> */}
     </div>
   )
 }
